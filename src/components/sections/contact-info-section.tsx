7a0ae84954ad21c900b913181e816f4366555cbb
Jethro Cadang <jethrocadang@gmail.com>
2025-07-18 21:03:03 +0800

    feat: added base layout for featured projects

--- src/components/sections/contact-info-section.tsx
+++ src/components/sections/contact-info-section.tsx
@@ -1,9 +1,16 @@
+import { Button } from "../ui/button";
 
-
-export function ContactInfoSection () {
-    return (
-        <section>
-            Get in touch
-        </section>
-    )
+export function ContactInfoSection() {
+  return (
+    <section className="flex h-full items-center justify-center">
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="text-3xl font-bold">Get in Touch.</h1>
+        <p> Currently I am open for job offer, feel free to contact me via email. Looking forward for your message!</p>
+        <div className="space-x-2.5">
+          <Button>Send Email</Button>
+          <Button>Curicullum Vitae</Button>
+        </div>
+      </div>
+    </section>
+  );
 }
