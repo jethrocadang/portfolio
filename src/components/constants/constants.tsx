1ac4a5dd872e3aa4e356be750d575f3195ce5fd2
Jethro Cadang <jethrocadang@gmail.com>
2025-07-23 00:05:24 +0800

    chore: updated constants

--- src/components/constants/constants.tsx
+++ src/components/constants/constants.tsx
@@ -37,9 +37,9 @@ export interface IProject {
   title: string;
   description: string;
   technologies: string[];
-  contributors: IContributor[];
+  contributors?: IContributor[];
   github: string;
-  link: string;
+  link?: string;
   thumbnailUrl: string;
 }
 
@@ -74,55 +74,142 @@ export const technologies = [
 ];
 
 export const projects: IProject[] = [
-  // {
-  //   title: "Applicant Tracking and Employee Training System",
-  //   description:
-  //     "A final requirement for capstone project. This system is partial part of HRIS focusing on ATS and ETH. The system is built on a layered architecrure with React JS client & Laravel API server.",
-  //   technologies: [" Vite", "React JS", "Laravel", "Spatie", "MySQl"],
-  //   contributors: [
-  //     {
-  //       id: 1,
-  //       image: "/jossa.JPG",
-  //       name: "Jossa Marie Advincula ",
-  //       role: "Project Manager & UI/UX",
-  //     },
-  //     {
-  //       id: 2,
-  //       image: "/jossa.JPG",
-  //       name: "Jethro R Cadang",
-  //       role: "Full-stack Developer",
-  //     },
-  //     {
-  //       id: 3,
-  //       image: "/erish.JPG",
-  //       name: "John Erish Berboso",
-  //       role: "Back-end Developer",
-  //     },
-  //     {
-  //       id: 4,
-  //       image: "/umali.JPG",
-  //       name: "John Michael Umali",
-  //       role: "Front-end Developer",
-  //     },
-  //     {
-  //       id: 5,
-  //       image: "/jem.JPG",
-  //       name: "Jemyll Ramoya",
-  //       role: "UI Designer",
-  //     },
-  //   ],
-  //   github: "https://github.com/mickeyumali/lvcc-hris-frontend",
-  //   thumbnailUrl: "./placeholder.svg",
-  //   link: "#",
-  // },
+  {
+    title: "Personal Portfolio",
+    description:
+      "A modern, responsive web application designed to showcase professional projects and technical expertise.",
+    technologies: [
+      "Next JS",
+      "Typescript",
+      "Tailwind CSS",
+      "ShadcnUI",
+      "RadixUI",
+      "Framer Motion",
+      "Vercel",
+    ],
+    contributors: [
+      {
+        id: 1,
+        image: "/jethro.JPG",
+        name: "Jethro R Cadang",
+        role: "Full-stack Developer",
+      },
+    ],
+    github: "https://github.com/jethrocadang/portfolio",
+    thumbnailUrl: "/portfolio.png",
+    link: "https://jethrocadang.vercel.app/",
+  },
+  {
+    title: "Applicant Tracking and Employee Training System",
+    description:
+      "A comprehensive Human Resource Information System (HRIS) solution focusing on Applicant Tracking System (ATS) and Employee Training Hub (ETH) functionalities. Built with a layered architecture using React.js frontend and Laravel API backend.",
+    technologies: [" Vite", "React JS", "Laravel", "Spatie", "MySQl"],
+    contributors: [
+      {
+        id: 1,
+        image: "/jossa.JPG",
+        name: "Jossa Marie Advincula ",
+        role: "Project Manager & UI/UX",
+      },
+      {
+        id: 2,
+        image: "/jethro.JPG",
+        name: "Jethro R Cadang",
+        role: "Full-stack Developer",
+      },
+      {
+        id: 3,
+        image: "/erish.JPG",
+        name: "John Erish Berboso",
+        role: "Back-end Developer",
+      },
+      {
+        id: 4,
+        image: "/umali.JPG",
+        name: "John Michael Umali",
+        role: "Front-end Developer",
+      },
+      {
+        id: 5,
+        image: "/jem.JPG",
+        name: "Jemyll Ramoya",
+        role: "UI Designer",
+      },
+    ],
+    github: "https://github.com/mickeyumali/lvcc-hris-frontend",
+    thumbnailUrl: "/atets.png",
+  },
   {
     title: "ConnectED",
     description:
-      "A web application contributing to the Quality Education SDG, and serves as a centralized repository of internships and scholarships in the Philippines.",
+      "A comprehensive web platform contributing to Quality Education SDG by serving as a centralized repository for internship opportunities and scholarship programs across the Philippines.",
     github: "https://github.com/Kertsu/LV-ConnectED",
-    link: "#",
     thumbnailUrl: "/connected.webp",
-    technologies: [ "React JS", "Typescript", "Tailwind CSS", "Express JS", "TypeORM", "ShadcnUI", "Radix UI", "Framer Motion", "Axios", "React Router"],
+    technologies: [
+      "React JS",
+      "Typescript",
+      "Tailwind CSS",
+      "Express JS",
+      "TypeORM",
+      "ShadcnUI",
+      "Radix UI",
+      "Framer Motion",
+      "Axios",
+      "React Router",
+    ],
+    contributors: [
+      {
+        id: 1,
+        name: "Kurtd Daniel Bigtas",
+        image: "/kurtd.jpeg",
+        role: "Full-stack Developer",
+      },
+      {
+        id: 2,
+        name: "Raven Dela Rama",
+        image:
+          "https://res.cloudinary.com/drlztlr1m/image/upload/v1729331139/Hackathon/olsermjsgxepfzalyzur.jpg",
+        role: "Full-stack Developer",
+      },
+      {
+        id: 3,
+        name: "Allen Magadia",
+        image:
+          "https://res.cloudinary.com/drlztlr1m/image/upload/v1735027541/Hackathon/eqnna3fxoebpd64a1hve.jpg",
+        role: "UI/UX Designer & Front-end Developer",
+      },
+      {
+        id: 4,
+        name: "Johnmack Faeldonia",
+        image:
+          "https://res.cloudinary.com/drlztlr1m/image/upload/v1729331139/Hackathon/xb9t23uznqfop14olmvl.jpg",
+        role: "Project Manager & Front-end Developer",
+      },
+      {
+        id: 5,
+        name: "Jethro Cadang",
+        image:
+          "https://res.cloudinary.com/drlztlr1m/image/upload/v1729331140/Hackathon/wpxmy7np9h5dfryp0not.jpg",
+        role: "Front-end Developer",
+      },
+    ],
+  },
+  {
+    title: "DeskFlow",
+    description:
+      "An innovative web application designed to facilitate organizational transitions to hybrid work environments. The platform provides comprehensive tools for workspace management and employee scheduling.",
+    github: "https://github.com/jethrocadang/desk-flow-hdbs-v2",
+    link: "https://desk-flow-hdbs-v2.vercel.app/",
+    thumbnailUrl: "/deskflow.png",
+    technologies: [
+      "Next JS",
+      "Typescript",
+      "Tailwind CSS",
+      "Shadcn UI",
+      "Vercel",
+      "Mongo DB",
+      "Prisma",
+    ],
     contributors: [
       {
         id: 1,
