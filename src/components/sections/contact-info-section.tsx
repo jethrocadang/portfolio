4c9795d20d396e3e6ac993259b1d63b4500973dc
Jethro Cadang <jethrocadang@gmail.com>
2025-07-23 16:46:44 +0800

    chore: added contact section

--- src/components/sections/contact-info-section.tsx
+++ src/components/sections/contact-info-section.tsx
@@ -1,14 +1,32 @@
-import { Button } from "../ui/button";
+import Link from "next/link";
+import { buttonVariants } from "../ui/button";
 
 export function ContactInfoSection() {
   return (
-    <section className="flex h-full items-center justify-center">
-      <div className="flex flex-col items-center justify-center">
-        <h1 className="text-3xl font-bold">Get in Touch.</h1>
-        <p> Currently I am open for job offer, feel free to contact me via email. Looking forward for your message!</p>
+    <section className="flex min-h-screen items-center justify-center">
+      <div className="flex flex-col items-center justify-center space-y-2.5">
+        <h1 className="text-4xl font-bold">Get in Touch.</h1>
+        <p className="text-center">
+          Currently I am open for job offer, feel free to contact me via email
+          or you can view my{" "}
+          <Link
+            href={"/jethrocadang_cv.pdf"}
+            className="text-primary font-bold underline underline-offset-1"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            credentials.
+          </Link>
+          <br />
+          Looking forward for your message!
+        </p>
         <div className="space-x-2.5">
-          <Button>Send Email</Button>
-          <Button>Curicullum Vitae</Button>
+          <Link
+            href={"mailto:jethrocadang@gmail.com"}
+            className={buttonVariants({ variant: "default" })}
+          >
+            Send Email
+          </Link>
         </div>
       </div>
     </section>
